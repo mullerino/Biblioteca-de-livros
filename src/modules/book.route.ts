22c18d64d6b10bb252fd243b22f07c54002dc29e
Leandro <leandromllr9@gmail.com>
2023-02-23 12:03:31 -0300

    feat: criando rotas de get, post e delete

--- src/modules/book.route.ts
+++ src/modules/book.route.ts
@@ -1,7 +1,25 @@
 import { FastifyInstance } from "fastify";
-import { testingHandler } from "./books.controller";
+import { allBooksHandler, deleteBookHandler, registerBookHandler } from "./books.controller";
+import { $ref } from "./book.schema";
 
 export const bookRoutes = async (server: FastifyInstance)=>{
     server.get("/", {
-    }, testingHandler)
+    }, allBooksHandler)
+
+    server.post("/register", {
+        schema: {
+            body: $ref('createBookSchema'),
+            response: {
+                201: $ref('BookSchemaResponse')
+            }
+        }
+    }, registerBookHandler)
+
+    server.delete("/:id", {
+        schema: {
+            response: {
+                201: $ref('BookSchemaResponse')
+            }
+        }
+    }, deleteBookHandler)
 }
